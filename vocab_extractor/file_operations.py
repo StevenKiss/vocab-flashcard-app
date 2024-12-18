78c97fe607387740d830be82c33daba1be5bc0b7
StevenKiss <kisssteven291@gmail.com>
2024-12-17 17:50:05 -0700

    Added environment to develop app with expo go. Reorganzied file processing code

--- vocab_extractor/file_operations.py
+++ vocab_extractor/file_operations.py
@@ -5,12 +5,17 @@ def save_vocab_to_file(file_path, vocab_list):
     Saves the extracted vocab to a file in structured manner
     """
 
-    # Split into base_name and extension
+    # Create an output directory if it doesn't exist
+    output_dir = os.path.join(os.path.dirname(__file__), "output_files")
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Generate the output file path
     base_name, ext = os.path.splitext(os.path.basename(file_path))
     new_file_name = f"{base_name}_extracted_vocab.txt"
+    output_file_path = os.path.join(output_dir, new_file_name)
 
-    # Save into file
-    with open(new_file_name, "w", encoding="utf-8") as f:
+    # Save the vocab into the file
+    with open(output_file_path, "w", encoding="utf-8") as f:
         for entry in vocab_list:
             print(entry)
             f.write(f"Word: {entry['Word']}\n")
