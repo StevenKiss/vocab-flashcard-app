dee1fb5ac6f6de06a4b67d4e2c510bf06ca490df
StevenKiss <kisssteven291@gmail.com>
2024-12-23 18:55:33 -0700

    New flashcards formatting complete as well as other miniture fixes. Code still not working fully but wanted to update anyways

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import { RootStackParamList } from './types/types'; // Import the navigation types
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 import {Ionicons} from '@expo/vector-icons'; // For icons
@@ -11,7 +12,8 @@ import {COLORS} from './constants/colors'; // To implement color scheme
 // Import the screens
 import HomeScreen from './screens/HomeScreen';
 import LibraryScreen from './screens/LibraryScreen';
-import FlashCardScreen from './screens/FlashCardScreen';
+import FlashcardScreen from './screens/FlashcardScreen';
+import FlashcardSettingsScreen from './screens/FlashcardSettingsScreen';
 import AddScreen from './screens/AddScreen';
 import CharactersScreen from './screens/CharactersScreen';
 import ProfileScreen from './screens/ProfileScreen';
@@ -36,8 +38,15 @@ function LibraryStackScreen() {
       {/* Flashcard is the secondary screen */}
       <LibraryStack.Screen 
         name="Flashcard" 
-        component={FlashCardScreen}
+        component={FlashcardScreen}
         options={({route}) => ({title: route.params?.title || 'Flashcard Set'})}
+      />
+
+      {/* FlaschcardSettings is the third screen*/}
+      <LibraryStack.Screen
+        name="FlashcardSettings"
+        component={FlashcardSettingsScreen}
+        options={{ title: 'Flashcard Settings' }}
       />
     </LibraryStack.Navigator>
   );
@@ -45,41 +54,43 @@ function LibraryStackScreen() {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={({route}) => ({ 
-        headerShown: false, // Hides the screen header making it look nice
-        tabBarStyle: {
-          backgroundColor: COLORS.background,
-          borderTopColor: COLORS.border,
-        },
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName;
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({route}) => ({ 
+          headerShown: false, // Hides the screen header making it look nice
+          tabBarStyle: {
+            backgroundColor: COLORS.background,
+            borderTopColor: COLORS.border,
+          },
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
 
-          // Give icons for each route
-          if (route.name == 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name == 'Library') {
-            iconName = focused ? 'book' : 'book-outline';
-          } else if (route.name == 'Add') {
-            iconName = focused ? 'add-circle' : 'add-circle-outline';
-          } else if (route.name == 'Characters') {
-            iconName = focused ? 'grid' : 'grid-outline';
-          } else if (route.name == 'Profile') {
-            iconName = focused ? 'person' : 'person-outline';
-          }
-        
-        return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: COLORS.primary, // Tab color when in use
-        tabBarInactiveTintColor: COLORS.textSecondary, // Tab color when not in use
-        })}
-      >
-        <Tab.Screen name= "Home" component={HomeScreen} />
-        <Tab.Screen name= "Library" component={LibraryStackScreen} />
-        <Tab.Screen name= "Add" component={AddScreen} />
-        <Tab.Screen name= "Characters" component={CharactersScreen} />
-        <Tab.Screen name= "Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+            // Give icons for each route
+            if (route.name == 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name == 'Library') {
+              iconName = focused ? 'book' : 'book-outline';
+            } else if (route.name == 'Add') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
+            } else if (route.name == 'Characters') {
+              iconName = focused ? 'grid' : 'grid-outline';
+            } else if (route.name == 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+          
+          return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: COLORS.primary, // Tab color when in use
+          tabBarInactiveTintColor: COLORS.textSecondary, // Tab color when not in use
+          })}
+        >
+          <Tab.Screen name= "Home" component={HomeScreen} />
+          <Tab.Screen name= "Library" component={LibraryStackScreen} />
+          <Tab.Screen name= "Add" component={AddScreen} />
+          <Tab.Screen name= "Characters" component={CharactersScreen} />
+          <Tab.Screen name= "Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
--- mobile_app/screens/FlashCardScreen.tsx
+++ mobile_app/screens/FlashCardScreen.tsx
@@ -1,70 +1,181 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import Flashcard from '../components/Flashcard';
-import { RootStackParamList } from '../types/types';
-import { RouteProp, useRoute } from '@react-navigation/native';
-
-type FlashcardRouteProp = RouteProp<RootStackParamList, 'Flashcard'>;
-
-type VocabItem = {
-    Word: string;
-    Pinyin: string;
-    Definition: string;
-}
+import React, {useState, useRef, useEffect} from 'react';
+import {View, Text, StyleSheet, Button, TouchableOpacity, Animated, Platform, StatusBar} from 'react-native';
+import Swiper from 'react-native-deck-swiper';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const FlashcardScreen = () => {
-    const route = useRoute<FlashcardRouteProp>();
-    const {vocab} = route.params;
+    const route = useRoute();
+    const navigation = useNavigation();
+    const {vocab} = route.params; // Vocab taken from LibraryScreen
 
-    const [frontContent, setFrontContent] = useState('Word');
-    const [backContent, setBackContent] = useState('Definition');
+    const [knownWords, setKnownWords] = useState([]);
+    const [unknownWords, setUnknownWords] = useState(vocab);
+    const [frontContent, setFrontContent] = useState('Word'); // Default: Chinese Character
+    const [backContent, setBackContent] = useState('Definition'); // Default: English
+    const [isFront, setIsFront] = useState(true); //Tracks the card side
+    const [progress, setProgress] = useState(0);
+
+    // Progress bar logic
+    useEffect(() => {
+        const total = vocab.length;
+        const completed = knownWords.length + (vocab.length - unknownWords.length);
+        const calculatedProgress = total > 0 ? completed / total : 0;
+        setProgress(calculatedProgress); // Avoid floating-point issues
+    }, [knownWords, unknownWords]);
+
+    // Animation for flipping card
+    const flipAnim = useRef(new Animated.Value(0)).current;
+
+    // Interpolations for the front and back of the card
+    const frontInterpolate = flipAnim.interpolate({
+        inputRange: [0, 180],
+        outputRange: ['0deg', '180deg'],
+    });
+
+    const backInterpolate = flipAnim.interpolate({
+        inputRange: [0, 180],
+        outputRange: ['180deg', '0deg'],
+    });
+
+    // To make sure flipAnim is in sync with isFront
+    useEffect(() => {
+        console.log(`isFront changed: ${isFront}`);
+        flipAnim.setValue(isFront ? 0 : 180); // Ensure the animation starts correctly
+    }, [isFront]);
+
+    let isFlipping = false;
+    // Flipping Card logic
+    const flipCard = () => {
+        if (isFlipping) return;
+        isFlipping = true;
+    
+        const toValue = isFront ? 180 : 0;
+    
+        Animated.timing(flipAnim, {
+            toValue, // Rotate to 180 or back to 0
+            duration: 300,
+            useNativeDriver: true,
+        }).start(() => {
+            isFlipping = false;
+            setIsFront(!isFront); // Toggle `isFront` only after animation completes
+        });
+    };    
+
+    // Swipping Handling
+    const handleSwipeRight = (cardIndex) => {
+        if (cardIndex >= 0 && cardIndex < unknownWords.length) {
+            const word = unknownWords[cardIndex];
+            setKnownWords((prev) => [...prev, word]);
+            setUnknownWords((prev) => prev.filter((_,i) => i !== cardIndex));
+            resetCardToFront(); // Resets the card to front side after swiping
+        }
+    };
+
+    const handleSwipeLeft = (cardIndex) => {
+        if (cardIndex >= 0 && cardIndex < unknownWords.length) {
+            setUnknownWords((prev) => prev.filter((_, i) => i !== cardIndex));
+            console.log('Word not known:', unknownWords[cardIndex]);
+            resetCardToFront(); // Resets the card to the front side after swiping
+        }
+    };
+
+    /* FOR WHEN I WILL HAVE TO DEBUG FLASHCARD FLIPPING IMPROPERLY
+    flipAnim.addListener(({value}) => {
+        console.log(`flipAnim value: ${value}`);
+    });
+    */
+
+    // Reseting card to front handling
+    const resetCardToFront = () => {
+        flipAnim.setValue(0);
+        setIsFront(true);
+    }
 
     return (
-        <View style={styles.container}>
-            {/* Drop Down selector for front content*/}
-            <View style={styles.selectorContainer}>
-                <Text style={[styles.label, {marginTop: 30}]}>Front Content:</Text>
-                <View style={styles.pickerWrapper}>
-                    <Picker
-                        selectedValue={frontContent}
-                        onValueChange={(value) => setFrontContent(value)}
-                        style={styles.picker}
-                    >
-                        <Picker.Item label="Chinese Character" value="Word" />
-                        <Picker.Item label="Pinyin" value="Pinyin" />
-                        <Picker.Item label="English Definition" value="Definition" />
-                    </Picker>
-                </View>
-            </View>
+        <View style={styles.appContainer}>
+            <SafeAreaView style={styles.safeArea}>
+                <StatusBar
+                    translucent
+                    backgroundColor="transparent"
+                    barStyle="dark-content"
+                />
+                <View style={styles.container}>
+                    {/* Header Section */}
+                    <View style={styles.header}>
+                        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+                            <Text style={styles.buttonText}>Back</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.progressText}>
+                            {knownWords.length + (vocab.length - unknownWords.length)}/{vocab.length}
+                        </Text>
+                        <TouchableOpacity
+                            style={styles.settingsButton}
+                            onPress={() =>
+                                navigation.navigate('FlashcardSettingsScreen', {
+                                    frontContent,
+                                    backContent,
+                                    setFrontContent,
+                                    setBackContent,
+                                })
+                            }
+                        >
+                            <Text style={styles.buttonText}>Settings</Text>
+                        </TouchableOpacity>
+                    </View>
 
-            {/* Drop Down selector for back content*/}
-            <View style={styles.selectorContainer}>
-                <Text style={styles.label}>Back Content:</Text>
-                <View style={styles.pickerWrapper}>
-                    <Picker
-                        selectedValue={backContent}
-                        onValueChange={(value) => setBackContent(value)}
-                        style={styles.picker}
-                        itemStyle={styles.pickerItem} 
-                    >
-                        <Picker.Item label="Chinese Character" value="Word" />
-                        <Picker.Item label="Pinyin" value="Pinyin" />
-                        <Picker.Item label="English Definition" value="Definition" />
-                    </Picker>
-                </View>
-            </View>
+                    {/* Custom Progress Bar */}
+                    <View style={styles.progressBarContainer}>
+                        <View style={[styles.progressBarFill, {width: `${progress * 100}%`}]} />
+                    </View>
 
-            {/*View scrollable flashcards*/}
-            <ScrollView contentContainerStyle={styles.flashcardContainer}>
-                {vocab.map((item, index) => (
-                    <Flashcard
-                        key={index}
-                        frontContent={item[frontContent as keyof VocabItem]}
-                        backContent={item[backContent as keyof VocabItem]}
-                    />
-                ))}
-            </ScrollView>
+                    {/* Known and Unknown Counts */}
+                    <View style={styles.statsContainer}>
+                        <Text style={styles.unknownText}>{unknownWords.length}</Text>
+                        <Text style={styles.knownText}>{knownWords.length}</Text>
+                    </View>
+
+                    {/* Flashcard Section */}
+                    <View style={styles.flashcardContainer}>
+                        <Swiper
+                            key={unknownWords.length}
+                            cards={unknownWords}
+                            renderCard={(card) => (
+                                <View style={styles.cardWrapper}>
+                                    <Animated.View
+                                        style={[
+                                            styles.card,
+                                            styles.cardFront,
+                                            {transform: [{rotateY: frontInterpolate}]},
+                                        ]}
+                                    >
+                                        <TouchableOpacity style={styles.cardTouchable} onPress={flipCard}>
+                                            <Text style={styles.cardText}>{card[frontContent]}</Text>
+                                        </TouchableOpacity>
+                                    </Animated.View>
+
+                                    <Animated.View
+                                        style={[
+                                            styles.card,
+                                            styles.cardBack,
+                                            {transform: [{rotateY: backInterpolate}]},
+                                        ]}
+                                    >
+                                        <TouchableOpacity style={styles.cardTouchable} onPress={flipCard}>
+                                            <Text style={styles.cardText}>{card[backContent]}</Text>
+                                        </TouchableOpacity>
+                                    </Animated.View>
+                                </View>
+                            )}
+                            onSwipedRight={(cardIndex) => handleSwipeRight(cardIndex)}
+                            onSwipedLeft={(cardIndex) => handleSwipeLeft(cardIndex)}
+                            cardIndex={0}
+                            backgroundColor="#EFE7EC"
+                            stackSize={3}
+                        />
+                    </View>
+                </View>
+            </SafeAreaView>
         </View>
     );
 };
@@ -72,41 +183,110 @@ const FlashcardScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
         backgroundColor: '#EFE7EC',
     },
-    selectorContainer: {
+    flashcardContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+    cardWrapper: {
+        flex: 0.6,
+        justifyContent: 'center',
+        alignItems: 'center',
         marginVertical: 10,
     },
-    label: {
-        fontSize: 16,
-        color: '#6F4E7C',
-        fontWeight: 'bold',
-        marginBottom: 5,
-    },
-    pickerWrapper: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: 10,
-        elevation: 2,
-        height: 120,
+    card: {
+        position: 'absolute',
+        width: 300,
+        height: 400,
         justifyContent: 'center',
-        textAlign: 'center',
-        overflow: 'hidden',
-    },
-    picker: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-    },
-    pickerItem: {
-        fontSize: 20,
-        textAlign: 'center',
-        color: '#000',
-    },
-    flashcardContainer: {
         alignItems: 'center',
-        paddingVertical: 20,
+        backfaceVisibility: 'hidden',
+        borderRadius: 10,
+        padding: 20,
+        backgroundColor: '#FFFFFF',
+        elevation: 4,
     },
+    cardFront: {
+        backgroundColor: '#FFFFFF'
+    },
+    cardBack: {
+        backgroundColor: '#FFE4B5',
+    },
+    cardTouchable: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cardText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 15,
+        paddingVertical: 0,
+        marginBottom: 0,
+        zIndex: 1, // Ensure above Swiper
+    },
+    progressText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#6F4E7C',
+    },
+    backButton: {
+        padding: 10,
+    },
+    settingsButton: {
+        padding: 10,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#6F4E7C',
+    },
+    progressBarContainer: {
+        height: 5,
+        backgroundColor: '#E0e0e0',
+        marginHorizontal: 15,
+        marginBottom: 15,
+        marginVertical: 10,
+        borderRadius: 3,
+        zIndex: 1, // Ensure above Swiper
+    },
+    progressBarFill: {
+        height: '100%',
+        backgroundColor: '#6F4E7C',
+        borderRadius: 3,
+    },
+    statsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 25,
+        zIndex: 1, // Ensure above Swiper
+    },
+    unknownText: {
+        fontSize: 18,
+        color: 'red',
+        fontWeight: 'bold',
+    },
+    knownText: {
+        fontSize: 18,
+        color: 'green',
+        fontWeight: 'bold',
+    },
+    safeArea: {
+        flex: 1,
+        backgroundColor: 'EFE7EC',
+    },
+    appContainer: {
+        flex: 1,
+        backgroundColor: '#EFE7EC',
+    }
 });
 
 export default FlashcardScreen;
